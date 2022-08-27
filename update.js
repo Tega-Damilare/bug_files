3e3852231123edcd65f3f00d361c6b20c7334573
Tega-Damilare <109291577+Tega-Damilare@users.noreply.github.com>
2022-08-27 05:31:26 +0100

    Add files via upload
    corrected errors fetching values from the local storage

--- update.js
+++ update.js
@@ -5,29 +5,72 @@
 
 let button = document.getElementById("saveBtn");
 let serviceName = document.getElementById("serviceName");
-let newPrice = document.getElementById("midder");
+// let newPrice = document.getElementById("midder");
+let newPrice = document.getElementById("inpt");
 let services = [];
 services.push(serviceName.value)
 button.addEventListener('click',(ev)=>{
-    let count = Number(localStorage.getItem("count"))
+    let price = newPrice.value
+    let serviceType = serviceName.value
+    let count = Number(localStorage.getItem("count"));
+    console.log(count)
     // console.log(count);
     // console.log(typeof(count))
-   let service = localStorage.getItem(JSON.parse(`new service${count}`))
-   services.push(service)
-   console.log(service);
-   console.log(services);
-   services.map((items)=>{
-       console.log(services);
-       console.log(items);
-       if (items.serviceName === serviceName) {
-           items.filter((services)=>{
-            console.log(services.unitPrice);
-            console.log(services.serviceName);
-            services.unitPrice = newPrice;
-            let updatedService = service;
-            localStorage.removeItem(`new service${count}`)
-            localStorage.setItem(`new service${count}`,JSON.stringify(updatedService))
-           })
-       }
-   })
+
+    //loop through the localstorage
+    for(let i = 0;i<localStorage.length;i++){
+        console.log(localStorage.length)
+        let service = JSON.parse(localStorage.getItem((`new service${i}`)))
+        services.push(service)
+        console.log((((services[i].serviceName))));
+        console.log(serviceType)
+   
+        console.log(services);
+        // services.map((items)=>{
+        //     console.log(services);
+        //     console.log(items);
+        services.filter((service)=>{
+
+            if (service.serviceName === serviceType) {
+                console.log('i was here')
+                console.log(service.unitPrice);
+                console.log(service.serviceName);
+                service.unitPrice = price;
+                console.log(service);
+                // let updatedService = service;
+                // localStorage.removeItem(`new service${count}`)
+                // localStorage.setItem(`new service${count}`,JSON.stringify(updatedService))
+            };
+        })
+    }    
+
+   ev.preventDefault();
 })
+
+
+class Animal {
+    constructor(name,size,age,color) {
+      this.name = name;
+      this.size = size;
+      this.age = age;
+      this.color = color
+    }
+  
+    speak() {
+      console.log(`${this.name} makes a noise.`);
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name,size,age,color) {
+      super(name,size,age,color); // call the super class constructor and pass in the name parameter
+    
+    }
+  
+    // speak() {
+    //   console.log(`${this.name} barks.`);
+    // }
+  }
+  
+  let dog1 = new Dog('bingo',12,2,'white')
+  console.log(dog1.speak())
